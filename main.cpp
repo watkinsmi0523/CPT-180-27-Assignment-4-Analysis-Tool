fdd29dbc2bac8efb6ee8842b9b273e5aa7bde577
Michael Watkins <watkinsmi@my.wccc.edu>
2016-03-21 20:28:25 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -115,7 +115,52 @@ int main(int argc, char *argv[]){
             outFile.close();
 
         }else if (static_cast<string>(argv[2]) == "count"){
+            ifstream inFile;
+            inFile.open(file_path);
 
+            if(inFile.is_open()){
+            const int SIZE = 36;
+
+                char letters[SIZE] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
+                                        's','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+
+                int amount[SIZE] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+                while(!inFile.eof()){
+                    char data_file;
+                    inFile >> data_file;
+                    for (int i = 0; i < SIZE; i++) {
+                        if (data_file == letters[i]){
+                            amount[i]++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < SIZE - 1; i++){
+                    for(int j = 0; j < SIZE - i - 1; j++){
+                        if(amount[i] > amount[i + 1]){
+                            int temp = amount[j];
+                            amount[j] = amount[j + 1];
+                            amount[j + 1] = temp;
+                           char temp2 = letters[j];
+                            letters[j] = letters[j + 1];
+                            letters[j + 1] = temp2;
+                        }
+                    }
+                }
+
+                ofstream outFile;
+                outFile.open("/count");
+
+                outFile << "Most used character is " << letters[0] << " used " << amount[0] << " times" << endl;
+                outFile << "Least used character is " << letters[36] << " used " << amount[36] << " times" << endl;
+
+                inFile.close();
+                outFile.close();
+
+            }
+        }
 
 
     }
